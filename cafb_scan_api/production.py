87e3ad3d9a96474930723c2142b533550e330599
ayota <elaine.ayo@gmail.com>
2016-07-25 15:54:32 -0400

    fix admin static files

--- cafb_scan_api/production.py
+++ cafb_scan_api/production.py
@@ -125,11 +125,11 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'cafb_scan_api/static/'
+STATIC_URL = 'staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 ]
 
 # Simplified static file serving.
