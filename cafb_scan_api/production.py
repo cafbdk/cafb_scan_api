2ef63b5b0780e9993b83a85903433cd188772dd7
ayota <elaine.ayo@gmail.com>
2016-07-25 15:59:19 -0400

    meeeeeeeeeeeeeeeeeeeeeeeeh

--- cafb_scan_api/production.py
+++ cafb_scan_api/production.py
@@ -124,8 +124,8 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'staticfiles/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
