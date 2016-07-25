b5a6394a4238604c5141375f599d11eb8630ef3e
ayota <elaine.ayo@gmail.com>
2016-07-25 15:08:20 -0400

    maybe!

--- cafb_scan_api/wsgi.py
+++ cafb_scan_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafb_scan_api.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafb_scan_api.production")
 
 application = get_wsgi_application()
